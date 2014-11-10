51d7e566e8dc1ef7b3f97c1cd44088dc9aaf2b01
Gene Blanchard <gblanchard4@gmail.com>
2014-11-10 15:19:26 -0600

    Now with more batchfiles

--- secondary_analysis.py
+++ secondary_analysis.py
@@ -18,6 +18,10 @@ Python secondary analysis script
 
 This script takes the basic primary analysis files (biom, map, tre, params) and runs the SOP commands
 
+TODO:
+Processors
+qiime17/18 shell scripts instead of all the individual Popens
+
 '''
 # Function to make sure qiime is loaded in the PATH
 def check_qiime(log):
@@ -48,98 +52,60 @@ def map_to_dictionary(mapfile):
 	return dictionary
 
 # Save the biom table stats to a file 
-def create_biom_summary(biom_file, log):
-	command = "print_biom_table_summary.py -i %s -o table_summary.txt" % biom_file
-	log.write(command+'\n')
-	biom_proc = subprocess.Popen(command.split(' '))
-	biom_proc.wait()
+def create_biom_summary(biom_file, batch):
+	command = "print_biom_table_summary.py -i {} -o table_summary.txt".format(biom_file)
+	batch.write(command+'\n')
 
-
-def qiime17_otu_category_sig(biom, mapfile, categories, log):
-	tests = ['g_test', 'ANOVA']
-	temp_stamp = str(int(time.time()))
-	with open('otu_category_significance_batch_'+temp_stamp, 'w') as batch:
-		batch.write('#!/bin/bash\nsource /media/nfs_opt/qiime17/activate.sh\n')
-		for category in categories:
-			for test in tests:
-				otu_cat_sig_command = "otu_category_significance.py -i %s -m %s -c %s -s %s -o taxa_summary/taxa_%s/%s.txt&" % (biom, mapfile, category, test, category, test)
-				batch.write(otu_cat_sig_command+'\n')
-				log.write(otu_cat_sig_command+'\n')
-	os.chmod('otu_category_significance_batch_'+temp_stamp, 0755)
-	otu_cat_sig_proc = subprocess.Popen(['./otu_category_significance_batch_'+temp_stamp])
-	otu_cat_sig_proc.wait()
-
-def summarize_taxa(biom, mapfile, params, categories, log):
-	# Make Taxa Summary Directory
-	os.mkdir('taxa_summary')
-	basic_taxa_command = "summarize_taxa_through_plots.py -s -i %s -m %s -p %s -o taxa_summary/taxa_individual" % (biom, mapfile, params)
-	log.write(basic_taxa_command+'\n')
-	basic_taxa_proc = subprocess.Popen(basic_taxa_command.split(' '))
-	basic_taxa_proc.wait()
+def summarize_taxa(biom, mapfile, params, categories, batch):
+	basic_taxa_command = "summarize_taxa_through_plots.py -s -i {} -m {} -p {} -o taxa_summary/taxa_individual".format(biom, mapfile, params)
+	batch.write(basic_taxa_command+'\n')
 	for category in categories:
-		category_taxa_command = "summarize_taxa_through_plots.py -i %s -m %s -p %s -o taxa_summary/taxa_%s -c %s -s" % (biom, mapfile, params, category, category)
-		log.write(category_taxa_command+'\n')
-		category_taxa_proc = subprocess.Popen(category_taxa_command.split(' '))
-		category_taxa_proc.wait()
+		category_taxa_command = "summarize_taxa_through_plots.py -i {} -m {} -p {} -o taxa_summary/taxa_{} -c {} -s".format(biom, mapfile, params, category, category)
+		batch.write(category_taxa_command+'\n')
 
-def alpha_diversity(biom, mapfile, params, tre, log):
-	alpha_command = "alpha_rarefaction.py -i %s -m %s -p %s -t %s -a -O 24 -o alpha_diversity" % (biom, mapfile, params, tre)
-	log.write(alpha_command+'\n')
-	alpha_proc = subprocess.Popen(alpha_command.split(' '))
-	alpha_proc.wait()
+def alpha_diversity(biom, mapfile, params, tre, batch):
+	alpha_command = "alpha_rarefaction.py -i {} -m {} -p {} -t {} -a -O 24 -o alpha_diversity".format(biom, mapfile, params, tre)
+	batch.write(alpha_command+'\n')
 
-def compare_alpha_diversity(cwd, mapfile, categories, log):
+def compare_alpha_diversity(mapfile, categories, batch):
 	alpha_diversity_path = 'alpha_diversity/alpha_div_collated/'
 	metrics = ['chao1', 'observed_species', 'PD_whole_tree', 'shannon', 'simpson']
-	temp_stamp = str(time.time())[:10]
-	batchfile_name = alpha_diversity_path+'batch'+temp_stamp
 	for metric in metrics:
 		for category in categories:
 			metric_file = alpha_diversity_path+metric
 			output =  alpha_diversity_path+metric+'_'+category
-			compare_alpha_diversity_command = "compare_alpha_diversity.py -m %s -n 9999 -c %s -i %s.txt -o %s &\n" % (mapfile, category, metric_file, output)
-			with open(batchfile_name, 'a') as batchfile:
-				batchfile.write(compare_alpha_diversity_command)
-				log.write(compare_alpha_diversity_command)
-	compare_alpha_diversity_proc = subprocess.Popen(['sh',batchfile_name])
-	compare_alpha_diversity_proc.wait()
+			compare_alpha_diversity_command = "compare_alpha_diversity.py -m {} -n 9999 -c {} -i {}.txt -o {} \n".format(mapfile, category, metric_file, output)
+			batch.write(compare_alpha_diversity_command)
 
-def beta_diversity(biom, mapfile, tre, log):
-	beta_diversity_command = "beta_diversity_through_plots.py -i %s -m %s -t %s -o beta_diversity" % (biom, mapfile, tre)
-	log.write(beta_diversity_command+'\n')
-	beta_diversity_proc =  subprocess.Popen(beta_diversity_command.split(' '))
-	beta_diversity_proc.wait()
+def beta_diversity(biom, mapfile, tre, batch):
+	beta_diversity_command = "beta_diversity_through_plots.py -i {} -m {} -t {} -o beta_diversity".format(biom, mapfile, tre)
+	batch.write(beta_diversity_command+'\n')
 	# Make the 2d plots
-	make_2d_unweighted_command = "make_2d_plots.py -i beta_diversity/unweighted_unifrac_pc.txt -m %s -o beta_diversity/2d_unweighted_unifrac_plots" % mapfile
-	make_2d_weighted_command = "make_2d_plots.py -i beta_diversity/weighted_unifrac_pc.txt -m %s -o beta_diversity/2d_weighted_unifrac_plots" % mapfile
-	log.write(make_2d_unweighted_command+'\n')
-	log.write(make_2d_weighted_command+'\n')
-	make_2d_unweighted_proc = subprocess.Popen(make_2d_unweighted_command.split(' '), stderr=log)
-	make_2d_unweighted_proc.wait()
-	make_2d_weighted_proc = subprocess.Popen(make_2d_weighted_command.split(' '), stderr=log)
-	make_2d_weighted_proc.wait()
-
-def compare_beta(mapfile, categories, log):
+	make_2d_unweighted_command = "make_2d_plots.py -i beta_diversity/unweighted_unifrac_pc.txt -m {} -o beta_diversity/2d_unweighted_unifrac_plots".format(mapfile)
+	make_2d_weighted_command = "make_2d_plots.py -i beta_diversity/weighted_unifrac_pc.txt -m {} -o beta_diversity/2d_weighted_unifrac_plots".format(mapfile)
+	batch.write(make_2d_unweighted_command+'\n')
+	batch.write(make_2d_weighted_command+'\n')
+	
+def compare_beta(mapfile, categories, batch):
 	for category in categories:
-		compare_beta_unweighted_command = "compare_categories.py --method anosim -i beta_diversity/unweighted_unifrac_dm.txt -m %s -c %s -n 9999 -o beta_diversity/ANOSIM_%s_unweighted" % (mapfile, category, category)
-		compare_beta_weighted_command = "compare_categories.py --method anosim -i beta_diversity/weighted_unifrac_dm.txt -m %s -c %s -n 9999 -o beta_diversity/ANOSIM_%s_weighted" % (mapfile, category, category)
-		log.write(compare_beta_unweighted_command+'\n')
-		log.write(compare_beta_weighted_command+'\n')
-		compare_beta_unweighted_proc = subprocess.Popen(compare_beta_unweighted_command.split(' '))
-		compare_beta_unweighted_proc.wait()
-		compare_beta_weighted_proc = subprocess.Popen(compare_beta_weighted_command.split(' '))
-		compare_beta_weighted_proc.wait()
-
-def compute_core_microbiome(biom, mapfile, categories, log):
-	os.mkdir('core_microbiome')
+		compare_beta_unweighted_command = "compare_categories.py --method anosim -i beta_diversity/unweighted_unifrac_dm.txt -m {} -c {} -n 9999 -o beta_diversity/ANOSIM_{}_unweighted".format(mapfile, category, category)
+		compare_beta_weighted_command = "compare_categories.py --method anosim -i beta_diversity/weighted_unifrac_dm.txt -m {} -c {} -n 9999 -o beta_diversity/ANOSIM_{}_weighted".format(mapfile, category, category)
+		batch.write(compare_beta_unweighted_command+'\n')
+		batch.write(compare_beta_weighted_command+'\n')
+		
+def compute_core_microbiome(biom, mapfile, categories, batch):
 	state_dictionary = map_to_dictionary(mapfile)
 	for category in categories:
 		for state in state_dictionary[category]:
-			core_microbiome_command = 'compute_core_microbiome.py -i %s --mapping_fp %s --num_fraction_for_core_steps 6 -o core_microbiome/core_microbiome_%s_%s --valid_states %s:%s' % (biom, mapfile, category, state, category, state)
-			log.write(core_microbiome_command+'\n')
-			core_microbiome_proc = subprocess.Popen(core_microbiome_command.split(' '))
-			core_microbiome_proc.wait()
+			core_microbiome_command = "compute_core_microbiome.py -i {} --mapping_fp {} --num_fraction_for_core_steps 6 -o core_microbiome/core_microbiome_{}_{} --valid_states {}:{}".format(biom, mapfile, category, state, category, state)
+			batch.write(core_microbiome_command+'\n')
 			
+def qiime17_otu_category_sig(biom, mapfile, categories, batch):
+	tests = ['g_test', 'ANOVA']
+	for category in categories:
+		for test in tests:
+			otu_cat_sig_command = "otu_category_significance.py -i {} -m {} -c {} -s {} -o taxa_summary/taxa_{}/{}.txt".format(biom, mapfile, category, test, category, test)
+			batch.write(otu_cat_sig_command+'\n')
 
 def get_time():
 	now = datetime.datetime.now().ctime()
@@ -183,8 +149,8 @@ def main():
 	mapping_file = args.map
 	params_file = args.params
 	tre_file =  args.tre
-	qiime18 =  args.qiime18
-	qiime17 = args.qiime17 
+	qiime18_source =  args.qiime18
+	qiime17_source = args.qiime17 
 
 	# Make sure a valid parameters file has been input
 	required_values =['summarize_taxa:level','plot_taxa_summary:labels','alpha_diversity:metrics','multiple_rarefactions:min','multiple_rarefactions:max','multiple_rarefactions:step','beta_diversity_through_plots:seqs_per_sample']
@@ -196,7 +162,7 @@ def main():
 	for required in required_values:
 		if required not in found_values:
 			print required+" not found in parameters file.\n"
-			print "\nI.E.:\nsummarize_taxa:level 2,3,4,5,6,7\nplot_taxa_summary:labels Phylum,Class,Order,Family,Genus,Species\nalpha_diversity:metrics shannon,simpson,PD_whole_tree,chao1,observed_species\nmultiple_rarefactions:min 100\nmultiple_rarefactions:max 18000\nmultiple_rarefactions:step 500\nbeta_diversity_through_plots:seqs_per_sample 18000\n"
+			print valid_params
 			sys.exit()
 	valid_params = """
 summarize_taxa:level 2,3,4,5,6,7
@@ -227,115 +193,53 @@ beta_diversity_through_plots:seqs_per_sample 18000
 
 	# Check Qiime paths (Make sure we get a '0' return code when calling
 	# Qiime 18
-	if subprocess.check_call(["sh",qiime18]) == 0:
+	if subprocess.check_call(["sh",qiime18_source]) == 0:
 		pass
 	else:
 		print "Check your Qiime 1.8 path. Non-zero return code returned when trying to source it."
-		print "Path:\t{}".format(qiime18)
+		print "Path:\t{}".format(qiime18_source)
 	# Qiime 17
-	if subprocess.check_call(["sh",qiime17]) == 0:
+	if subprocess.check_call(["sh",qiime17_source]) == 0:
 		pass
 	else:
 		print "Check your Qiime 1.7 path. Non-zero return code returned when trying to source it."
-		print "Path:\t{}".format(qiime17)
+		print "Path:\t{}".format(qiime17_source)
 
 	# Command lists
-	qiime18_command_list = ["source {};".format(qiime18)]
-	qiime17_command_list = ["source {};".format(qiime17)]
+	# Qiime18
+	with open('qiime18.sh', 'w') as qiime18:
+		# Source the enviroment
+		qiime18.write("#!/bin/bash\nsource {};\n".format(qiime18_source))
+		# Biom table summary
+		create_biom_summary(biom_file, qiime18)	
+		# Mkdir Taxa summary <<<<<<<<<<
+		qiime18.write("mkdir taxa_summary/\n")
+		# Taxa summary commands
+		summarize_taxa(biom_file, mapping_file, params_file, categories, qiime18)
+		# Alpha div
+		alpha_diversity(biom_file, mapping_file, params_file, tre_file, qiime18)
+		# Alpha div colated
+		compare_alpha_diversity(mapping_file, categories, qiime18)
+		# Beta diversity
+		beta_diversity(biom_file, mapping_file, tre_file, qiime18)
+		# Comapre categories 
+		compare_beta(mapping_file, categories, qiime18)
+		# Core microbiome
+		qiime18.write("mkdir core_microbiome/\n")
+		compute_core_microbiome(biom_file, mapping_file, categories, qiime18)
+	# Qiime 17
+	with open('qiime17.sh', 'w') as qiime17:
+		# Source the enviroment
+		qiime17.write("#!/bin/bash\nsource {};\n".format(qiime17_source))
+		# OTU Category Sig
+		qiime17_otu_category_sig(biom_file, mapping_file, categories, qiime17)
 
 
-	time_stamp = str(int(time.time()))
-	logfile = 'secondary_batch_%s.log' % time_stamp
-
-	
-	with open(logfile, 'w') as log:
-
-		# Print Categories
-		log.write("Categories:"+str(categories)+'\n')
-		
-		# Check qiime in path
-		# start = time.time()
-		# log.write("Check if Qiime is in $PATH\t"+get_time())
-		# check_qiime(log)
-		# elapsed = time.time() - start
-		# hms_elapsed = "Elapsed {}\n\n".format(hms_string(elapsed))
-		# log.write(hms_elapsed)
-		# log.flush()		
-		
-		# Summarize BIOM table
-		start = time.time()
-		log.write("BIOM Table Summary\t"+get_time())
-		create_biom_summary(biom_file, log)
-		elapsed = time.time() - start
-		hms_elapsed = "Elapsed {}\n\n".format(hms_string(elapsed))
-		log.write(hms_elapsed)
-		log.flush()
-		
-		# Summarize taxa through plots
-		start = time.time()
-		log.write("Summarize Taxa\t"+get_time())
-		summarize_taxa(biom_file, mapping_file, params_file, categories, log)
-		elapsed = time.time() - start
-		hms_elapsed = "Elapsed {}\n\n".format(hms_string(elapsed))
-		log.write(hms_elapsed)
-		log.flush()
-		
-		# Compute Alpha diversity
-		start = time.time()
-		log.write("Alpha Diversity\t"+get_time())
-		alpha_diversity(biom_file, mapping_file, params_file, tre_file, log)
-		elapsed = time.time() - start
-		hms_elapsed = "Elapsed {}\n\n".format(hms_string(elapsed))
-		log.write(hms_elapsed)
-		log.flush()
-		
-		# Compare Alpha diversity
-		start = time.time()
-		log.write("Compare Alpha Diversity\t"+get_time())
-		compare_alpha_diversity(cwd, mapping_file, categories, log)
-		elapsed = time.time() - start
-		hms_elapsed = "Elapsed {}\n\n".format(hms_string(elapsed))
-		log.write(hms_elapsed)
-		log.flush()
-		
-		# Compute Beta diversity
-		start = time.time()
-		log.write("Beta Diversity\t"+get_time())
-		beta_diversity(biom_file, mapping_file, tre_file, log)
-		elapsed = time.time() - start
-		hms_elapsed = "Elapsed {}\n\n".format(hms_string(elapsed))
-		log.write(hms_elapsed)
-		log.flush()
-		
-		# Compare Beta Diversity
-		start = time.time()
-		log.write("Compare Beta Diversity\t"+get_time())
-		compare_beta(mapping_file, categories, log)
-		elapsed = time.time() - start
-		hms_elapsed = "Elapsed {}\n\n".format(hms_string(elapsed))
-		log.write(hms_elapsed)
-		log.flush()
-		
-		# Compute core microbiome
-		start = time.time()
-		log.write("Core Microbiome\t"+get_time())
-		compute_core_microbiome(biom_file, mapping_file, categories, log)
-		elapsed = time.time() - start
-		hms_elapsed = "Elapsed {}\n\n".format(hms_string(elapsed))
-		log.write(hms_elapsed)
-		log.flush()
-		
-		# OTU category sig
-		start = time.time()
-		log.write("OTU Category Significance\t"+get_time())
-		qiime17_otu_category_sig(biom_file, mapping_file, categories, log)
-		elapsed = time.time() - start
-		hms_elapsed = "Elapsed {}\n\n".format(hms_string(elapsed))
-		log.write(hms_elapsed)
-		log.flush()
-
-
-
+	# Run qiime18 commands
+	qiime18_proc = subprocess.Popen("./qiime18.sh", shell=True)
+	qiime18_proc.wait()
+	qiime17_proc = subprocess.Popen("./qiime17.sh", shell=True)
+	qiime17_proc.wait()
 
 if __name__ == "__main__":
 	main()
